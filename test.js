2993523055aab362fb60cb6ce87d3ca56e81bada
SpoiledPotato <itulashvili@yahoo.com>
2021-06-23 06:34:09 +0400

    complete main menu

--- test.js
+++ test.js
@@ -1,3 +1,12 @@
+document.getElementById("communal").src = "images/კომუნალური.png";
+document.getElementById("tel-int-tv").src = "images/ტელეფონი_ინტერნეტი_ტელევიზია.png";
+document.getElementById("mobile").src = "images/მობილური.png";
+document.getElementById("transport").src = "images/სატრანსპორტო_სერვისები.png";
+
 function changeMenu(x) {
-    location.href=x+".html"
+    location.href = x + ".html"
+}
+
+function hover(id, y) {
+    document.getElementById(id).src = "images/" + y + ".png";
 }
