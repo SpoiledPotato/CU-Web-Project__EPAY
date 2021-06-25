c3e32c5bb7600170463ec42e5d546c27e0a83034
SpoiledPotato <itulashvili@yahoo.com>
2021-06-25 04:39:37 +0400

    complete gwp page

--- test.js
+++ test.js
@@ -1,7 +1,10 @@
-document.getElementById("communal").src = "images/კომუნალური.png";
-document.getElementById("tel-int-tv").src = "images/ტელეფონი_ინტერნეტი_ტელევიზია.png";
-document.getElementById("mobile").src = "images/მობილური.png";
-document.getElementById("transport").src = "images/სატრანსპორტო_სერვისები.png";
+function loadMain() {
+    document.getElementById("communal").src = "images/კომუნალური.png";
+    document.getElementById("tel-int-tv").src = "images/ტელეფონი_ინტერნეტი_ტელევიზია.png";
+    document.getElementById("mobile").src = "images/მობილური.png";
+    document.getElementById("transport").src = "images/სატრანსპორტო_სერვისები.png";
+}
+
 
 function changeMenu(x) {
     location.href = x + ".html"
@@ -9,4 +12,20 @@ function changeMenu(x) {
 
 function hover(id, y) {
     document.getElementById(id).src = "images/" + y + ".png";
+}
+
+function generateLiabilities() {
+    var davalianeba = Math.floor(Math.random() * 10000) / 100;
+    document.getElementById("davalianeba").value = davalianeba + "₾";
+
+    var Tanxa = davalianeba + ((davalianeba * 2) / 100);
+    document.getElementById("Tanxa").value = Math.round(Tanxa * 100) / 100 + "₾";
+}
+
+function toCart() {
+    alert("დამატებულია კალათაში");
+}
+
+function test() {
+    alert("WORKED!")
 }
